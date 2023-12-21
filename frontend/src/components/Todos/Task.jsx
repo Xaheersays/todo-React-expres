c92246fce51b36be1f7f4162946fa2146fa09e9f
Xaheersays <shzaheer514@gmail.com>
2023-12-21 19:08:17 +0530

    responsive,small changes

--- frontend/src/components/Todos/Task.jsx
+++ frontend/src/components/Todos/Task.jsx
@@ -3,15 +3,18 @@ import dlt from '../../assets/bin.png'
 import right from '../../assets/checkmark.png'
 function Task() {
   return (
-    <div className='text-white p-4  mt-2'>
+    <div className='text-white p-4  mt-2 '>
         <div>
-            <div className='flex  justify-center items-center'>
-                <input className='cursor-pointer' type="checkbox" name="" id="" />
-                <div className='ml-3 p-2 w-full bg-yellow-200 rounded-md outline-none text-black font-mono font-bold text-xl'
-                contentEditable
-                >kkajkdjaksjd</div>
-                <img className='h-9 ml-3 cursor-pointer rounded-tr-md rounded-br-md  active:scale-95 text-white   rounded focus:outline-none focus:shadow-outline' src={right} alt="" />
-                <img className='h-8 ml-3 cursor-pointer rounded-tr-md rounded-br-md  active:scale-95 text-white   rounded focus:outline-none focus:shadow-outline' src={dlt}></img>
+            <div className='flex flex-wrap  justify-evenly'>
+                <div className=' flex  w-2/5 md:w-3/4'>
+                    <input className='cursor-pointer' type="checkbox" name="" id="" />
+                    <div className='ml-3 p-2  w-full  bg-yellow-100 rounded-md outline-none text-black font-mono   md:font-bold md:text-xl text-sm font-light'
+                    contentEditable></div>
+                </div>
+                <div className='flex justify-center items-center gap-2'>
+                    <img className='h-9  cursor-pointer rounded-tr-md rounded-br-md  active:scale-95 text-white   rounded focus:outline-none focus:shadow-outline' src={right} alt="" />
+                    <img className='h-8  cursor-pointer rounded-tr-md rounded-br-md  active:scale-95 text-white   rounded focus:outline-none focus:shadow-outline' src={dlt}></img>
+                </div>
             </div>
         </div>
     </div>
--- frontend/src/components/Todos/Todo.jsx
+++ frontend/src/components/Todos/Todo.jsx
@@ -3,7 +3,7 @@ import TakeInput from './TakeInput'
 import Task from './Task'
 function Todo() {
   return (
-    <div className='md:w-9/12 bg-slate-600 bg-opacity-50 backdrop-filter backdrop-blur-md  rounded-md'>
+    <div className='md:w-9/12 bg-slate-600 bg-opacity-50 backdrop-filter backdrop-blur-md  rounded-md md:mx-w-9/12 '>
         <TakeInput/>
         <Task/>
         <Task/>
